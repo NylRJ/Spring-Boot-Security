a58daad28aa7cb2661f4576045d28697a2e27ea6
Moises <loveloungeoficial@gmail.com>
2020-05-04 23:17:59 -0300

    Implementar ação de logoff - autenticação de formulários

--- securityBasecHttp/src/main/java/com/i9Developed/i9/security/SecurityConfiguration.java
+++ securityBasecHttp/src/main/java/com/i9Developed/i9/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +43,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter  {
 				.antMatchers("/api/public/users").hasRole("ADMIN")
 				.and()
 				.formLogin()
-				.loginPage("/login").permitAll();
+				.loginPage("/login").permitAll()
+				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 				
 	}
 	
